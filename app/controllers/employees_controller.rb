7a9ae6fd16d1ead86c840165adee31cee7f44c14
Jaye Hackett <jayehackett@FutureGovs-MacBook-Pro.local>
2020-09-28 21:34:16 +0100

    add create and delete functionality

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -1,5 +1,6 @@
 class EmployeesController < ApplicationController
     before_action :set_employees, only: :index
+    before_action :set_employee, only: [:show, :destroy]
 
     def index
     end
@@ -7,14 +8,26 @@ class EmployeesController < ApplicationController
     def show
     end
 
+    def new
+        @employee = Employee.new
+    end
+
     def create
+        @employee = Employee.new(employee_params)
+        if @employee.save
+            redirect_to employees_path
+        else
+            render "new"
+        end
     end
 
     def update
     end
 
-
-    def delete
+    def destroy
+        if @employee.destroy
+            redirect_to employees_path
+        end
     end
 
     private
@@ -23,6 +36,17 @@ class EmployeesController < ApplicationController
         @employees = Employee.all
     end
 
+    def set_employee
+        @employee = Employee.find(params[:id])
+    end
+
     def employee_params
+        params.require(:employee).permit(
+            :last_name,
+            :other_names,
+            :role,
+            :employed_from,
+            :employed_to
+        )
     end
 end
