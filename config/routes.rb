921658a3551778c88ec73dfc01fa0a061bc5ccd9
Jaye Hackett <jayehackett@FutureGovs-MacBook-Pro.local>
2020-09-28 22:54:11 +0100

    tidy routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,8 @@
 Rails.application.routes.draw do
   root "flow#start"
-
   get "start", to: "flow#start", as: "start"
   get "eligibility", to: "flow#eligibility", as: "eligibility"
   post "eligibility", to: "flow#check_eligibility"
   get "confirmation", to: "flow#confirmation", as: "confirmation"
-
-
   resources :employees, except: :edit
 end
