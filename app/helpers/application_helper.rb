eff8aabe66cd62c31b99cdcbf527698a5f2fbf98
Jaye Hackett <jayehackett@FutureGovs-MacBook-Pro.local>
2020-10-08 15:15:09 +0100

    fix session bug

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+
+    def service_name_by_id(id)
+        results = session[:services].select{|s| s["id"] === id }
+        if results.any?
+            results.first["name"]
+        else
+            id
+        end
+    end
+    
 end
