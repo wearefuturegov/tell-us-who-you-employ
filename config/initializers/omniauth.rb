02c9b4602060f007ce4c98884a8cc25f61876f79
lizzie cullen davison <lizzie.cullendavison@gmail.com>
2021-11-16 18:15:45 +0000

    Try allowing OmniAuth GET requests

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,7 @@
 require "outpost"
 
+OmniAuth.config.allowed_request_methods = [:post, :get]
+
 Rails.application.config.middleware.use OmniAuth::Builder do
     provider :outpost, ENV["OAUTH_CLIENT_ID"], ENV["OAUTH_SECRET"]
 end
