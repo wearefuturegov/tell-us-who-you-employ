2e68ce0362b4942ad2efa0121c1ba128672d527c
gabimartini <gabimartinii@hotmail.com>
2022-05-05 14:44:43 +0100

    update test

--- spec/factories/employees.rb
+++ spec/factories/employees.rb
@@ -6,7 +6,7 @@ FactoryBot.define do
     postal_code { FFaker::AddressUK.postcode }
     date_of_birth { FFaker::Time.date(year_latest: 18, year_range: 65 - 18) }
     employed_from { FFaker::Time.date(year_range: 25) }
-    role { FFaker::Job.title }
+    job_title { FFaker::Job.title }
     currently_employed { true }
   end
 end
