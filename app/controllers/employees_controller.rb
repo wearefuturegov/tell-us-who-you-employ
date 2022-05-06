385dbc1a5c188e7e699e0c5350c023df91eb677c
gabimartini <gabimartinii@hotmail.com>
2022-05-06 17:18:07 +0100

    adding , to controller

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -65,7 +65,7 @@ class EmployeesController < ApplicationController
       :dbs_expires_at,
       :has_first_aid_training,
       :first_aid_expires_at,
-      :roles => []
+      :roles => [],
       :has_food_hygiene,
       :food_hygiene_achieved_on,
       :qualifications => []
