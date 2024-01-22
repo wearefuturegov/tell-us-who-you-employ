f639d1184fc50c8de605553708d604fbba12d342
Madeleine Williams <maddie.williams@tpximpact.com>
2024-01-22 15:55:47 +0000

    Employee detail page, with collapsible headers using JS

---
+++ app/assets/javascripts/collapsible.js
@@ -0,0 +1,28 @@
+document.addEventListener('DOMContentLoaded', (event) => {
+    document.querySelectorAll(".collapsible").forEach((collapsible, index) => {
+        let controls = collapsible.querySelector(".collapsible__header");
+        let content = collapsible.querySelector(".collapsible__content");
+        let key = `collapsible_${collapsible.id}`;
+
+        const open = () => {
+            controls.setAttribute("aria-expanded", "true");
+            content.style.display = "block";
+            window.localStorage.setItem(key, true);
+        };
+
+        const close = () => {
+            controls.setAttribute("aria-expanded", "false");
+            content.style.display = "none";
+            window.localStorage.setItem(key, false);
+        };
+
+        window.localStorage.getItem(key) === "true" && open()
+        window.localStorage.getItem(key) === "false" && close()
+
+        controls.addEventListener("click", e => {
+            e.preventDefault()
+            controls.getAttribute("aria-expanded") === "false" ? open() : close()
+
+        });
+    });
+});
--- app/controllers/admin/base_controller.rb
+++ app/controllers/admin/base_controller.rb
@@ -1,12 +1,2 @@
 class Admin::BaseController < ApplicationController
-  # before_action :require_admin!
-
-  # private
-
-  # def require_admin!
-  #   unless session[:admin]
-  #     redirect_to root_path
-  #   end
-  # end
-
 end
