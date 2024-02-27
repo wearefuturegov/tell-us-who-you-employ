0b6aea405afca64045073aac0a39e97b2dae67cf
Madeleine Williams <maddie.williams@tpximpact.com>
2024-02-27 14:15:37 +0000

    remove migration

--- db/migrate/20240226113032_remove_redundant_columns_from_employees.rb
+++
@@ -1,6 +0,0 @@
-class RemoveRedundantColumnsFromEmployees < ActiveRecord::Migration[6.1]
-  def change
-    remove_column :employees, :is_potential_duplicate, :boolean
-    remove_column :employees, :ignore_duplicate, :boolean
-  end
-end
